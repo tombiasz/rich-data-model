64f128b2ba5ca5439bfa82fc8b3324f08fc49653
tombiasz <tomasz.m.nowacki@gmail.com>
2019-05-30 22:05:33 +0200

    use shorter method names for readability

--- src/domain/uuid4.js
+++ src/domain/uuid4.js
@@ -2,26 +2,26 @@ const uuid4 = require('uuid4');
 
 class UUID4 {
   constructor(value = null) {
-    this.value = this.tryUUID4(value);
+    this.value = this.try(value);
   }
 
-  tryUUID4(value) {
+  try(value) {
     if (value === null) {
-      return this.generateUUID4();
+      return this.generate();
     }
 
-    if (this.isValidUUID4(value)) {
+    if (this.isValid(value)) {
       return value;
     }
 
     throw new Error('Invalid uuid4');
   }
 
-  isValidUUID4(value) {
+  isValid(value) {
     return uuid4.valid(value);
   }
 
-  generateUUID4() {
+  generate() {
     return uuid4();
   }
 }
