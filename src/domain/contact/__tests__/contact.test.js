10659833fdb092d6a59c9297c189fb09458f4445
tombiasz <tomasz.m.nowacki@gmail.com>
2019-07-14 19:39:30 +0200

    use factory to create Contact

--- src/domain/contact/__tests__/contact.test.js
+++ src/domain/contact/__tests__/contact.test.js
@@ -5,7 +5,7 @@ const now = 123456;
 const timeProvider = {
   now: () => now,
 };
-const validContactProps = {
+const makeContact = () => new Contact({
   id: '16357d7a-5738-4d8b-adc7-b8fa10cd5331',
   ownerId: '16357d7a-5738-4d8b-adc7-b8fa10cd5331',
   firstName: 'foo',
@@ -15,7 +15,7 @@ const validContactProps = {
   createdAt: 631152000,
   updatedAt: 631152000,
   archivedAt: null,
-};
+}, timeProvider);
 const validContactEmailProps = {
   emailId: '16357d7a-5738-4d8b-adc7-b8fa10cd5331',
   isStarred: false,
@@ -26,34 +26,34 @@ const validContactEmailProps = {
 describe('Contact entity', () => {
   describe('new instance', () => {
     test('should set initial attributes', () => {
-      const contact = new Contact(validContactProps);
-      expect(contact.id).toBe(validContactProps.id);
-      expect(contact.ownerId).toBe(validContactProps.ownerId);
-      expect(contact.firstName).toBe(validContactProps.firstName);
-      expect(contact.lastName).toBe(validContactProps.lastName);
-      expect(contact.description).toBe(validContactProps.description);
-      expect(contact.createdAt).toBe(validContactProps.createdAt);
-      expect(contact.updatedAt).toBe(validContactProps.updatedAt);
+      const contact = makeContact();
+      expect(contact.id).toBe('16357d7a-5738-4d8b-adc7-b8fa10cd5331');
+      expect(contact.ownerId).toBe('16357d7a-5738-4d8b-adc7-b8fa10cd5331');
+      expect(contact.firstName).toBe('foo');
+      expect(contact.lastName).toBe('bar');
+      expect(contact.description).toBe('fizz buzz');
+      expect(contact.createdAt).toBe(631152000);
+      expect(contact.updatedAt).toBe(631152000);
     });
   });
 
   describe('fullName', () => {
     test('should return first name and last name', () => {
-      const contact = new Contact(validContactProps);
+      const contact = makeContact();
       expect(contact.fullName).toBe('foo bar');
     });
   });
 
   describe('addEmail', () => {
     test('should add email to emails list', () => {
-      const contact = new Contact(validContactProps, timeProvider);
+      const contact = makeContact();
       expect(contact.emails.size()).toBe(0);
       contact.addEmail(validContactEmailProps);
       expect(contact.emails.size()).toBe(1);
     });
 
     test('should set updatedAt to now', () => {
-      const contact = new Contact(validContactProps, timeProvider);
+      const contact = makeContact();
       contact.addEmail(validContactEmailProps);
       expect(contact.updatedAt).toBe(now);
     });
@@ -61,7 +61,7 @@ describe('Contact entity', () => {
 
   describe('removeEmail', () => {
     test('should remove email from emails list', () => {
-      const contact = new Contact(validContactProps, timeProvider);
+      const contact = makeContact();
       contact.addEmail(validContactEmailProps);
       expect(contact.emails.size()).toBe(1);
       contact.removeEmail({ emailId: validContactEmailProps.emailId });
@@ -69,7 +69,7 @@ describe('Contact entity', () => {
     });
 
     test('should set updatedAt to now', () => {
-      const contact = new Contact(validContactProps, timeProvider);
+      const contact = makeContact();
       contact.addEmail(validContactEmailProps);
       contact.removeEmail(validContactEmailProps.emailId);
       expect(contact.updatedAt).toBe(now);
@@ -78,7 +78,7 @@ describe('Contact entity', () => {
 
   describe('touch()', () => {
     test('should set udatedAt to now', () => {
-      const contact = new Contact(validContactProps, timeProvider);
+      const contact = makeContact();
       contact.touch();
       expect(contact.updatedAt).toBe(now);
     });
