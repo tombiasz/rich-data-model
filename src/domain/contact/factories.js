047b029ec6dc51b57cb896bca8dd579047defcce
tombiasz <tomasz.m.nowacki@gmail.com>
2019-07-16 21:52:59 +0200

    introduce standard and premium contact factories

--- src/domain/contact/factories.js
+++ src/domain/contact/factories.js
@@ -2,9 +2,10 @@ const UUID4 = require('../uuid4');
 const AutogenerateUUID4 = require('../autogenerateUuid4');
 const Contact = require('./contact');
 const ContactEmailCollection = require('./contactEmailCollection');
+const ContactEmailCollectionPolicy = require('./contactEmailCollectionPolicy');
 const timeProvider = require('../../utils/timeProvider');
 
-const makeContact = ({
+const makeStandardContact = ({
   id,
   ownerId,
   firstName,
@@ -13,9 +14,39 @@ const makeContact = ({
   createdAt,
   updatedAt,
 }) => {
+  const standardPolicy = new ContactEmailCollectionPolicy(3);
   const _id = new UUID4(id);
   const _ownerId = new AutogenerateUUID4(ownerId);
-  const emails = new ContactEmailCollection(timeProvider);
+  const emails = new ContactEmailCollection(timeProvider, standardPolicy);
+  const _createdAt = createdAt || timeProvider.now();
+  const _updatedAt = updatedAt || timeProvider.now();
+
+  return new Contact({
+    id: _id.valie,
+    ownerId: _ownerId.value,
+    firstName,
+    lastName,
+    description,
+    emails,
+    createdAt: _createdAt,
+    updatedAt: _updatedAt,
+  },
+  timeProvider);
+};
+
+const makePremiumContact = ({
+  id,
+  ownerId,
+  firstName,
+  lastName,
+  description,
+  createdAt,
+  updatedAt,
+}) => {
+  const premiumPolicy = new ContactEmailCollectionPolicy(999);
+  const _id = new UUID4(id);
+  const _ownerId = new AutogenerateUUID4(ownerId);
+  const emails = new ContactEmailCollection(timeProvider, premiumPolicy);
   const _createdAt = createdAt || timeProvider.now();
   const _updatedAt = updatedAt || timeProvider.now();
 
@@ -33,5 +64,6 @@ const makeContact = ({
 };
 
 module.exports = {
-  makeContact,
+  makeStandardContact,
+  makePremiumContact,
 };
